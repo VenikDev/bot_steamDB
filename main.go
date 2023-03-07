d892e8b1486f8506da9ba3755a8cfc083a1e8ccc
Alexander <a.oshurkov@iridi.tech>
2023-03-07 12:33:28 +0500

    - add comments - add func Listen

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"bot_steamDB/src/clog"
 	"bot_steamDB/src/consts"
 	"bot_steamDB/src/handler_error"
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	tgBotApi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
 	"os"
 )
@@ -14,16 +14,24 @@ func main() {
 		herr.HandlerError(err, "No .env file found")
 	}
 	apiToken := os.Getenv(consts.NAME_API_TOKEN)
-	bot, err := tgbotapi.NewBotAPI(apiToken)
+	bot, err := tgBotApi.NewBotAPI(apiToken)
 	herr.HandlerFatal(err, "Token not correct")
 
 	bot.Debug = true
 
 	clog.Logger.Info("[BOT]", "Authorized on account", bot.Self.UserName)
 
-	updateConfig := tgbotapi.NewUpdate(0)
-	updateConfig.Timeout = 60
+	Listen(bot)
+}
 
+// Listen
+// This code listens for incoming messages in a chat bot API by creating an updates channel and continuously checking
+// for updates. If the update received is a message,
+// the code creates a new message using the received message's chat ID and text and sends it using the bot's send
+// function. Any errors that occur during the send process are logged.
+func Listen(bot *tgBotApi.BotAPI) {
+	updateConfig := tgBotApi.NewUpdate(0)
+	updateConfig.Timeout = 60
 	updates := bot.GetUpdatesChan(updateConfig)
 
 	// This code receives updates from a chat bot API and checks if the update is a message. If it is,
@@ -34,7 +42,7 @@ func main() {
 		if update.Message != nil { // If we got a message
 			clog.Logger.Info("[BOT MESSAGE]", update.Message.From.UserName, update.Message.Text)
 
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg := tgBotApi.NewMessage(update.Message.Chat.ID, update.Message.Text)
 			msg.ReplyToMessageID = update.Message.MessageID
 
 			_, err := bot.Send(msg)
